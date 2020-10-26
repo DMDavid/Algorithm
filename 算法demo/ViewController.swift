080436f9983c72fe0a792406bc29c78de059dff7
dumeng <dumeng1@tuhu.cn>
2020-10-26 17:06:19 +0800

    leetCode  1365. 【有多少小于当前数字的数字】

--- 算法demo/ViewController.swift
+++ 算法demo/ViewController.swift
@@ -13,11 +13,111 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("\(smallerNumbersThanCurrent2([5,0,10,0,10,6]))")
+    }
+}
+
+/*
+ 1365. 有多少小于当前数字的数字
+ 给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
+
+ 换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
+
+ 以数组形式返回答案。
+
+  
+
+ 示例 1：
+
+ 输入：nums = [8,1,2,2,3]
+ 输出：[4,0,1,1,3]
+ 解释：
+ 对于 nums[0]=8 存在四个比它小的数字：（1，2，2 和 3）。
+ 对于 nums[1]=1 不存在比它小的数字。
+ 对于 nums[2]=2 存在一个比它小的数字：（1）。
+ 对于 nums[3]=2 存在一个比它小的数字：（1）。
+ 对于 nums[4]=3 存在三个比它小的数字：（1，2 和 2）。
+ 示例 2：
+
+ 输入：nums = [6,5,4,8]
+ 输出：[2,1,0,3]
+ 示例 3：
+
+ 输入：nums = [7,7,7,7]
+ 输出：[0,0,0,0]
+  
+
+ 提示：
+
+ 2 <= nums.length <= 500
+ 0 <= nums[i] <= 100
+ */
+extension ViewController {
+    func smallerNumbersThanCurrent2(_ nums: [Int]) -> [Int] {
+        var resultDict: [Int: Int] = [:]
+        let newNums = nums.sorted()
+        var currentNum: Int = 0
+        var repeatNumCount: Int = 1  //出现重复的数字 +1
+        for (index, item) in newNums.enumerated() {
+            if currentNum == item {
+                var count = index - repeatNumCount
+                count = count < 0 ? 0 : count
+                resultDict[item] = count
+                repeatNumCount += 1
+            } else {
+                resultDict[item] = index
+                repeatNumCount = 1
+            }
+            currentNum = item
+        }
         
+        var result: [Int] = []
+        for item in nums {
+            result.append(resultDict[item]!)
+        }
+        return result
+    }
+    
+    func smallerNumbersThanCurrent(_ nums: [Int]) -> [Int] {
+        var resultList: [Int] = []
+        for item in nums {
+            
+            var count = 0
+            for item2 in nums {
+                if item == item2 {
+                    continue
+                }
+                if item > item2 {
+                    count += 1
+                }
+            }
+            resultList.append(count)
+        }
+        return resultList
+    }
+}
+
+/*
+ 请判断一个链表是否为回文链表。
+
+ 示例 1:
+
+ 输入: 1->2
+ 输出: false
+ 示例 2:
+
+ 输入: 1->2->2->1
+ 输出: true
+ 进阶：
+ 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/palindrome-linked-list
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+extension ViewController {
+    func test2() {
         
-        
-        
-//        print("\(isUnique("abc"))")
         let model = Solution()
         
         let one = ListNode(1)
@@ -30,7 +130,7 @@ class ViewController: UIViewController {
         let three4 = ListNode(3)
         let three = ListNode(-2)
         let four = ListNode(1)
-
+        
         one.next = two
         two.next = three1
         three1.next = three2
@@ -39,19 +139,15 @@ class ViewController: UIViewController {
         three4.next = three
         three.next = four
         
-//        let one = ListNode(1)
-//        let two = ListNode(2)
-//        one.next = two
+        //        let one = ListNode(1)
+        //        let two = ListNode(2)
+        //        one.next = two
         
-//        print("\(model.isPalindrome(one))")
+        //        print("\(model.isPalindrome(one))")
         
         print("\(isPalindrome(one))")
     }
-}
-
-
-
-extension ViewController {
+    
     public class ListNode {
         public var val: Int
         public var next: ListNode?
@@ -61,7 +157,7 @@ extension ViewController {
         }
     }
     
-    
+    // 遍历，中分，反转一个list，两个list比较
     class Solution {
         func isPalindrome(_ head: ListNode?) -> Bool {
             guard let testHeader = head else { return true }
@@ -98,7 +194,7 @@ extension ViewController {
         }
     }
 
-          
+        //快慢指针
     func isPalindrome(_ l1: ListNode?) -> Bool {
         if l1 == nil || l1?.next == nil {
             return false
